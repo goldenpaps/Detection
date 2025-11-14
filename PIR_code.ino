c7791f75fefb114bc34e4b11de8b62436334ad54
Zaythesmith <165862664+Zaythesmith@users.noreply.github.com>
2025-11-14 15:00:43 -0500

    Update PIR_code.ino
    fixed errors from in class

--- PIR_code.ino
+++ PIR_code.ino
@@ -1,26 +1,24 @@
 void setup() {
   pinMode(7,INPUT);
-  Serial.begin(9600);
+      Serial.begin(9600);
 }
 
 void loop(){
-    int sensor = 1;
+
+    int sensor = 0;
     int state = 0;
 
   sensor = digitalRead(7);
-  if (sensor == HIGH) {
-    if (state == 0){ //just turned on
-      Serial.println("On1");
-   
+  if (sensor == 1 && state == 0){
+    Serial.println("On1");
     state = 1;
-    }
-      else{} //been on
-  } 
-  else {
-    if (state == 1){ //just turned off
-    Serial.println("Off1");
-    state = 0;
-    }
-    else{}//been off
-  } 
+    delay(50);
+  }
+  else if (sensor ==1 && state == 1){
+
+  }
+  else{
+    Serial.println("OFF");
+    delay(50);
+  }
 }
